6cb4c182cbaa53cc36f0432fffb215ca40d8d934
alina-marushka <101595575+alina-marushka@users.noreply.github.com>
2022-05-14 16:47:04 +0300

    Merge remote-tracking branch 'origin/st-alina-marushka-lesson-2-level-1-task1' into st-alina-marushka

--- lesson1/HelloWorld.java
+++ lesson1/HelloWorld.java
@@ -2,7 +2,7 @@ package lesson1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println(" It works on this machine");
-        System.out.println(" Hello Artsem!!");
+        System.out.println(" Hello Artsem!!!");
 
     }
 }
