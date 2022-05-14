5e768d8515f58d7306b4cdda4c23392354de0178
alina-marushka <101595575+alina-marushka@users.noreply.github.com>
2022-05-14 16:21:40 +0300

    HomeWork

--- lesson1/HelloWorld.java
+++ lesson1/HelloWorld.java
@@ -1,8 +1,8 @@
-public class helloWorld {
+package lesson1;
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println(" It works on this machine");
         System.out.println(" Hello Artsem!!");
-
 
     }
 }
