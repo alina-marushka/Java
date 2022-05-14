3ef174d68f7c815263e30be76d78802581809951
alina-marushka <101595575+alina-marushka@users.noreply.github.com>
2022-05-14 16:21:40 +0300

    HomeWork

---
+++ lesson2/Task1.java
@@ -0,0 +1,24 @@
+package lesson2;
+
+public class Task1 {
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        //
+        System.out.println("Enter first number");
+        int firstNumber = scanner.nextInt();
+        //
+        System.out.println("Enter second number");
+        int secondNumber = scanner.nextInt();
+        //
+        int sumOfNumbers = firstNumber + secondNumber;
+        int subtractionOfNumbers = firstNumber - secondNumber;
+        int multiplicationOfNumbers = firstNumber * secondNumber;
+        double divisionOfNumbers = (double) firstNumber / (double) secondNumber;
+        //
+        System.out.println(firstNumber + " + " + secondNumber + " = " + sumOfNumbers);
+        System.out.println(firstNumber + " - " + secondNumber + " = " + subtractionOfNumbers);
+        System.out.println(firstNumber + " * " + secondNumber + " = " + multiplicationOfNumbers);
+        System.out.println(firstNumber + " / " + secondNumber + " = " + divisionOfNumbers);
+
+    }
+}
