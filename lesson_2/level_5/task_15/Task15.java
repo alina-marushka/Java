dbbf7963a79d8c1aa5627c15ec3de382bf5b16f8
alina-marushka <101595575+alina-marushka@users.noreply.github.com>
2022-06-04 20:02:54 +0300

    HomeWork

---
+++ lesson_2/level_5/task_15/Task15.java
@@ -0,0 +1,9 @@
+package lesson_2.level_5.task_15;
+
+public class Task15 {
+    public static void main(String[] args) {
+        System.out.println( "You can login!");
+        System.out.print("Hello Artsem!" );
+        System.out.print("How are you?");
+    }
+}
