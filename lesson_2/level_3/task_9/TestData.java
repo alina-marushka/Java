c5feb92f6b8bacd87c4d2873baa06968ca78de28
alina-marushka <101595575+alina-marushka@users.noreply.github.com>
2022-06-04 15:37:20 +0300

    HomeWork

---
+++ lesson_2/level_3/task_9/TestData.java
@@ -0,0 +1,39 @@
+package lesson_2.level_3.task_9;
+
+public class TestData {
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        //
+        System.out.println("Input a number:");
+
+        //
+        int Number = scanner.nextInt();
+        int multiplication = Number * 1;
+        int multiplication2 = Number * 2;
+        int multiplication3 = Number * 3;
+        int multiplication4 = Number * 4;
+        int multiplication5 = Number * 5;
+        int multiplication6 = Number * 6;
+        int multiplication7 = Number * 7;
+        int multiplication8 = Number * 8;
+        int multiplication9 = Number * 9;
+        int multiplication10 = Number * 10;
+
+        // вывод на экран
+        System.out.println(Number + "*" + 1 + "=" + multiplication);
+        System.out.println(Number + "*" + 2 + "=" + multiplication2);
+        System.out.println(Number + "*" + 3 + "=" + multiplication3);
+        System.out.println(Number + "*" + 4 + "=" + multiplication4);
+        System.out.println(Number + "*" + 5 + "=" + multiplication5);
+        System.out.println(Number + "*" + 6 + "=" + multiplication6);
+        System.out.println(Number + "*" + 7 + "=" + multiplication7);
+        System.out.println(Number + "*" + 8 + "=" + multiplication8);
+        System.out.println(Number + "*" + 9 + "=" + multiplication9);
+        System.out.println(Number + "*" + 10 + "=" + multiplication10);
+
+
+
+
+
+    }
+}
